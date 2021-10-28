c87d679c20bf7132d91527801ad266d06bbadec1
Gendalf4ever <60482216+Gendalf4ever@users.noreply.github.com>
2021-10-28 16:32:39 +0300

    основа подправлена
    Подправил шаблон. Теперь генерация случайного вопроса и ответы на кнопках

--- Assets/Scripts/Questions.cs
+++ Assets/Scripts/Questions.cs
@@ -2,19 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 public class Questions : MonoBehaviour
 {
     public QuestionList[] questions;
+    public Text[] answersOnButtonText;
     public Text questionText;
-
+    List<object> questionsList;
+    int randomQuestionNumber;
+   
     public void Game()
     {
-        questionText.text = questions[Random.Range(0, questions.Length)].question;
+        questionsList = new List<object>(questions);
+        questionGenerator();
+    }
+    public void answerButtonClick()
+    {
+        questionsList.RemoveAt(randomQuestionNumber);
+        questionGenerator();
+    }
+    public void questionGenerator()
+    {
+        randomQuestionNumber = Random.Range(0, questionsList.Count);
+        QuestionList currentQuestion = questionsList[randomQuestionNumber] as QuestionList;
+        questionText.text = currentQuestion.question;
+        for (int i = 0; i < currentQuestion.answers.Length; i++) answersOnButtonText[i].text = currentQuestion.answers[i];
+
     }
 }
 [System.Serializable]
 public class QuestionList
 {
     public string question;
-    public string[] answers = new string[4];
+    public string[] answers = new string[4]; //maybe 3
 }
