49ac6dd8e50a22d9d74ff8179809779a3596ca0c
Gendalf4ever <60482216+Gendalf4ever@users.noreply.github.com>
2021-10-29 19:57:09 +0300

    Счетчики уровня и количества попыток
    Реализован счетчик уровней, Прибавляется после правильного ответа. Нужен для последующей отправки в базу данных. Счетчик ошибок нужен для учета неправильных ответов

--- Assets/Scripts/Questions.cs
+++ Assets/Scripts/Questions.cs
@@ -8,28 +8,51 @@ public class Questions : MonoBehaviour
     public QuestionList[] questions;
     public Text[] answersOnButtonText;
     public Text questionText;
+    public Text level;
+    int levelCounter=1;
+    public Text numberOfTries;
+    int intNumberOfTries;
+    public Image[] questionImage = new Image[4];
     List<object> questionsList;
     int randomQuestionNumber;
     QuestionList currentQuestion;
     private void Start()
     {
         questionsList = new List<object>(questions);
+        level.text = levelCounter.ToString();
         questionGenerator();
     }
     public void answerButtonClick(int index)
     {
-        if (answersOnButtonText[index].text.ToString() == currentQuestion.answers[0]) print("Правильный ответ");
-        else print("Неправильный ответ");
-        questionsList.RemoveAt(randomQuestionNumber);
-        questionGenerator();
+        if (answersOnButtonText[index].text.ToString() == currentQuestion.answers[0])
+        {
+            print("Правильный ответ");
+            questionsList.RemoveAt(randomQuestionNumber);
+            levelCounter++;
+            level.text = levelCounter.ToString();
+            questionGenerator();
+        }
+        else
+        {
+            print("Неправильный ответ");
+            intNumberOfTries--;
+            numberOfTries.text = intNumberOfTries.ToString();
+
+        } 
+      
+
     }
-    public void questionGenerator()
+     void questionGenerator()
     {
         if (questionsList.Count > 0)
         {
+            intNumberOfTries = 4;
+            numberOfTries.text = intNumberOfTries.ToString();
             randomQuestionNumber = Random.Range(0, questionsList.Count);
             currentQuestion = questionsList[randomQuestionNumber] as QuestionList;
             questionText.text = currentQuestion.question;
+            //numberOfTries.text =intNumberOfTries.ToString();
+            //questionImage.sprite = currentQuestion.question;
             List<string> answers = new List<string>(currentQuestion.answers);
             for (int i = 0; i < currentQuestion.answers.Length; i++)
             {
@@ -47,5 +70,6 @@ public class Questions : MonoBehaviour
 public class QuestionList
 {
     public string question;
-    public string[] answers = new string[4]; //maybe 3
+    public string[] answers = new string[4];
+    public Sprite[] questionSprite = new Sprite[4];
 }
